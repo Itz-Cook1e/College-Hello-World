8bd0ab0b59c9cabdba56920d2ba4979437343cb8
Itz Cook1e <Asherpflueger@outlook.com>
2021-09-09 20:29:42 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Assignment: 
+# 1. Include a comment with your name, the date, and title of the program.
+# 2. Use a print statement to output "Hello, world!" on line 1.
+# 3. Use a print statement to output a second line of text of your choosing.
+
+# Printing lines
+print("Hello, world!")
+print("Hello, universe!")
+
+# Comment with name, date, and title redacted
